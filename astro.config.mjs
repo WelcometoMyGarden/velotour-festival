79f7329c9c69f4a374846b5dd78df932b4f0dfed
Thor Galle <thorgalle@gmail.com>
2026-01-08 17:04:52 +0100

    Fix Vercel deployment format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  build: {
+    // Align behavior with the prior GitHub Pages deployment
+    format: 'file'
+  }
 });
